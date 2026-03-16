a3afec0acc1360b172efeff07393d45f23a9510e
elsolcansado <leshka99514@gmail.com>
2026-03-16 12:07:55 +0300

    Добавлены константы

---
+++ config/constants.ts
@@ -0,0 +1,9 @@
+// Socials and contacts
+export const TG = "https://t.me/elsolcansado";
+export const INSTAGRAM = "https://www.instagram.com/elsolcansado/";
+export const RAW_PHONE = "+79507753918";
+export const FORMATTED_PHONE = "+7 (950) 775-39-18";
+
+
+
+export {}
